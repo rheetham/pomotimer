1d2e2df0c0c22bc2eb37fa47b623cc842cbce9d8
Yash Patil <rheethammenon@gmail.com>
2024-04-18 13:24:41 +0530

    added nav to allotted tasks

--- src/components/report/allotedTasks.jsx
+++ src/components/report/allotedTasks.jsx
@@ -2,6 +2,7 @@ import { onSnapshot, collection, query, orderBy } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../../config/firebase-config";
 import "./allotedtasks.css";
+import Navbar from "../navbar/navbar";
 
 function AllottedTasks() {
   const [tasks2, setTasks2] = useState([]);
@@ -22,6 +23,8 @@ function AllottedTasks() {
     };
   }, []);
   return (
+    <>
+    <Navbar />
     <div className="report">
       <div className="allottedTitle">Allotted Tasks</div>
       {tasks2.map((task, index) =>
@@ -33,6 +36,7 @@ function AllottedTasks() {
         ) : null
       )}
     </div>
+  </>
   );
 }
 
